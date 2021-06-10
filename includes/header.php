a97230a19a98189e56e4e7b341d44f0c4db2a5d1
Glaysa <glaysa.df@gmail.com>
2021-06-10 19:04:39 +0200

    updated nav bar

--- includes/header.php
+++ includes/header.php
@@ -7,10 +7,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
           rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
           crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/main.css">
     <title>Home</title>
 </head>
 
-<header class="sticky-top bg-dark">
+<header class="sticky-top bg-dark border-bottom border-secondary">
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
@@ -30,9 +32,14 @@
 
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav ms-auto">
-                        <a class="nav-link active" aria-current="page" href="#">En</a>
-                        <a class="nav-link" href="#">Search</a>
-                        <a class="nav-link" href="#">Menu</a>
+                        <a class="me-3 nav-link active" href="#">
+                            <span class="bi-book mx-2"></span>School</a>
+                        <a class="me-3 nav-link" href="#">
+                            <span class="bi-person mx-2"></span>Login</a>
+                        <a class="me-3 nav-link" href="#">
+                            <span class="bi-search mx-2"></span>Search</a>
+                        <a class="nav-link" href="#">
+                            <span class="bi-globe mx-2"></span>No</a>
                     </div>
                 </div>
             </div>
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 
 <?php include 'includes/header.php' ?>
 <body>
+<div class="container-fluid min-vh-100 bg-body-dark"></div>
+<div class="container-fluid min-vh-100 bg-body-dark"></div>
 </body>
 <?php include 'includes/footer.php' ?>
