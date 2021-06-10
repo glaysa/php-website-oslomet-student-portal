9ce244a29e03de5348d265261a3201657e6f7439
Glaysa <glaysa.df@gmail.com>
2021-06-10 13:56:50 +0200

    added bootstrap and jquery

--- includes/header.php
+++ includes/header.php
@@ -4,5 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+          rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
+          crossorigin="anonymous">
     <title>Home</title>
 </head>
