37055b97ca9c701e0ac513dbb1147c8b0bca31a1
Glaysa <glaysa.df@gmail.com>
2021-06-15 15:23:38 +0200

    added announcements section at student/index page

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <div class="text-lg-start text-center order-lg-1 order-2">
             <p class="display-6">Login</p>
             <hr class="w-100">
-            <p class="lead">Login to access your personal timetable, tools, services and other information.</p>
+            <p class="lead">Login to access your personal timetable, tools, services, announcements and other information.</p>
             <a href="login.php" class="btn btn-dark"><span class="bi-person-fill me-2"></span>LOGIN</a>
         </div>
         <div class="order-lg-2 order-1">
